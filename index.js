a62a91e0fd877e64682c855466809eeaac2ffebb
duronto0007 <satterhossain0@gmail.com>
2025-08-29 01:25:32 +0600

    call button & call history section has been done with fullfiling all the requirement

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ for (const heart of hearts) {
      
   });
 }
+
+// Call Buttons functionalities
+let coins = 100;
+const costPerCall = 20;
+
+const callButtons = document.getElementsByClassName('make-call');
+const totalCoins = document.getElementById('total-coins');
+const callHistoryList = document.getElementById('call-history');
+
+for(const button of callButtons){
+    button.addEventListener('click', function(){
+       const serviceName = button.parentNode.parentNode.parentNode.children[1].innerText
+       const serviceNumber = button.parentNode.parentNode.parentNode.children[3].innerText
+    //    console.log(serviceNumber);
+      if(coins < costPerCall){
+        alert('Not enough coins available to make this call')
+        return;
+      }
+      coins = coins - costPerCall;
+      totalCoins.innerText = coins;
+       
+      alert(`Calling ${serviceName} at ${serviceNumber}`)
+
+      const now = new Date();
+      const time = now.toLocaleTimeString();
+
+      const callingHistory = document.createElement('div');
+      callingHistory.innerHTML = `<div id="call-history" class="p-4 bg-gray-100 shadow mt-3">
+                <h2 class="font-bold">${serviceName}</h2>
+                <div class="flex justify-between">
+                    <h1>${serviceNumber}</h1>
+                    <p>${time}</p>
+                </div>
+            </div>`
+
+        callHistoryList.appendChild(callingHistory);
+
+    });
+    
+}
+const clearButton = document.getElementById('btn-clear').addEventListener('click', function(){
+    callHistoryList.innerText = "";
+})
